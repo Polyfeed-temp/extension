f64c03f364d6513fa5ec4401c866f1014baa9556
cooi0004 <cooi0004@student.monash.edu>
2023-10-12 19:58:12 +1100

    initial extension commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
-import "./App.css"
-
+import "./App.css";
+import {useState} from "react";
+import {highlighter} from "./store/highlighter.store";
 function App() {
+  const [counter, setCounter] = useState(0);
+  highlighter.run();
   return (
-    <div className="App">
-      Hello World
+    <div>
+      <button onClick={() => setCounter(counter + 1)}> increase</button>
+      <h1>{counter}</h1>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-const root = document.createElement("div")
-root.className = "container"
-document.body.appendChild(root)
+const root = document.createElement("div");
+root.id = "react-root";
+console.log("rendring react");
+document.body.appendChild(root);
+document.body.style.margin = "0px";
 const rootDiv = ReactDOM.createRoot(root);
 rootDiv.render(
   <React.StrictMode>
