0354c3d4008dedbec3ddebb26cecebf0ab792ff3
cooi0004 <cooi0004@student.monash.edu>
2023-10-19 01:57:58 +1100

    fix: clean up code

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,11 @@
 import "./App.css";
 import {useState, useEffect} from "react";
 import {DefaultSidebar} from "./components/Sidebar";
-import {useDispatch, useSelector} from "react-redux";
-import {
-  setEnable,
-  HighlighterState,
-  setCurrentID,
-} from "./store/highlightSlice";
 
 function App() {
-  const dispatcher = useDispatch();
-  const enable = useSelector((state: HighlighterState) => state.enable);
-  const id = useSelector((state: HighlighterState) => state.currentID);
-
-  useEffect(() => {
-    dispatcher(setCurrentID("123"));
-  }, [dispatcher]);
-
-  useEffect(() => {
-    console.log("Enable:", enable);
-  }, [enable]);
-
   return (
     <div>
       <DefaultSidebar />
-      <button onClick={() => dispatcher(setEnable(!enable))}>Enable</button>
     </div>
   );
 }
