0b4bef2d2f311000050dd300b4384df2b8443cb9
cooi0004 <cooi0004@student.monash.edu>
2023-10-26 11:47:51 +1100

    feat:added popup on annotated text

--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -7,18 +7,20 @@ import {
   Typography,
 } from "@material-tailwind/react";
 import {createRoot} from "react-dom/client";
-export function RenderPop({id, highlighter}: {id: string; highlighter: any}) {
-  console.log("rendering");
+import {createPortal} from "react-dom";
+import {useHighlighterState} from "../store/HighlightContext";
+export function RenderPop({id}: {id: string}) {
+  const highlighter = useHighlighterState().highlighterLib;
   const _id = `__highlight-${id}`;
   const el = document.getElementById(_id);
   const [visible, setVisible] = useState(true);
   const triggers = {
-    onMouseEnter: () => setVisible(true),
-    onMouseLeave: () => setVisible(false),
+    onmouseenter: () => setVisible(true),
+    onmouseleave: () => setVisible(false),
   };
 
   useEffect(() => {
-    const dom = highlighter.getDoms(id)[0];
+    const dom = highlighter?.getDoms(id)[0];
     const onClick = () => {
       setVisible(true);
     };
@@ -26,22 +28,24 @@ export function RenderPop({id, highlighter}: {id: string; highlighter: any}) {
       dom.addEventListener("click", onClick);
       return () => {
         dom.removeEventListener("click", onClick);
+        setVisible(false);
       };
     }
   }, [highlighter, id]);
-  useEffect(() => {
-    if (el) {
-      const root = createRoot(el);
-      root.render(
-        <Popover>
+  return el
+    ? createPortal(
+        <Popover open={visible} handler={setVisible}>
           <PopoverHandler {...triggers}>
-            <Button variant="text">Profile Info</Button>
+            <span></span>
           </PopoverHandler>
-          <PopoverContent {...triggers}> HII</PopoverContent>
-        </Popover>
-      );
-    }
-  }, [el]);
-
-  return null;
+          <PopoverContent {...triggers}>
+            <Button> Strength</Button> <Button> Weakness</Button>
+            <Button> Action Item</Button>
+            <Button>Confused</Button>
+            <Button>Other</Button>
+          </PopoverContent>
+        </Popover>,
+        el
+      )
+    : null;
 }
