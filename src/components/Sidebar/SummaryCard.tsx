e7c8b1d9d805c2217bc87dc4a3bb3c0bebc3792a
cooi0004 <cooi0004@student.monash.edu>
2023-11-02 10:25:21 +1100

    feat:added summary card for assignments

--- src/components/Sidebar/SummaryCard.tsx
+++ src/components/Sidebar/SummaryCard.tsx
@@ -1,33 +1,52 @@
 import React from "react";
-import {AnnotationData} from "../../types";
-
+import {AnnotationData, AnnotationTag, Unit} from "../../types";
+import {Card, Typography} from "@material-tailwind/react";
+import {annotationTagsIcons} from "../AnnotationIcons";
 interface Props {
   annotationData: AnnotationData[];
 }
 
+export function UnitAssignmentSummary({unit}: {unit: Unit}) {
+  return (
+    <div className="border rounded-lg bg-grey">
+      <div className="font-bold text-xl mb-4">{unit.unitCode}</div>
+      {unit.assignments.map((assignment) =>
+        assignment.feedback.annotations ? (
+          <SummaryCard annotationData={assignment.feedback.annotations} />
+        ) : null
+      )}
+    </div>
+  );
+}
+
 const SummaryCard: React.FC<Props> = ({annotationData}) => {
-  // const annotationTagCount:{AnnotationgTag:number} = {"strength":0}
-  // Group annotationData by category
-  const groupedData = annotationData.reduce((acc, curr) => {
-    const category = curr.annotation.AnnotationTag;
-    if (!acc[category]) {
-      acc[category] = [];
-    }
-    acc[category].push(curr);
-    return acc;
-  }, {} as {[key: string]: AnnotationData[]});
+  const annotationTagCount: {[key in AnnotationTag]: number} = {
+    Strength: 0,
+    Weakness: 0,
+    "Action Item": 0,
+    Confused: 0,
+    Other: 0,
+  };
+
+  annotationData.forEach(({annotation}) => {
+    annotationTagCount[annotation.AnnotationTag] += 1;
+  });
 
   // Render the grouped data
   return (
-    <div>
-      {Object.entries(groupedData).map(([category, data]) => (
-        <div key={category}>
-          <h3>{category}</h3>
-          <p>Count: {data.length}</p>
+    <div className="grid grid-cols-2 gap-4">
+      {Object.entries(annotationTagCount).map(([category, counter]) => (
+        <div key={category} className="flex items-center justify-between">
+          <div className="flex items-center">
+            <img
+              src={annotationTagsIcons[category as AnnotationTag]}
+              alt={category}
+              style={{width: 20, height: 20, marginRight: 8}}
+            />
+            {category + ": " + counter}
+          </div>
         </div>
       ))}
     </div>
   );
 };
-
-export default SummaryCard;
