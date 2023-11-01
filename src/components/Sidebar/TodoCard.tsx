1b00b31750b4ac677d18c69e228f7f78321a8d62
cooi0004 <cooi0004@student.monash.edu>
2023-11-02 10:50:40 +1100

    fix: stying for to do and saving

--- src/components/Sidebar/TodoCard.tsx
+++ src/components/Sidebar/TodoCard.tsx
@@ -1,12 +1,6 @@
 import React, {useState} from "react";
-
-type ToDoActionCategory =
-  | "Further Practice"
-  | "Contact Tutor"
-  | "Ask Classmate"
-  | "Refer Learning Resources"
-  | "Explore Online"
-  | "Other";
+import {AnnotationToDo} from "../../types";
+import {ToDoActionCategory} from "../../types";
 
 const ToDoActions: ToDoActionCategory[] = [
   "Further Practice",
@@ -17,28 +11,42 @@ const ToDoActions: ToDoActionCategory[] = [
   "Other",
 ];
 
-const TodoCard = () => {
+const TodoCard = ({setTodo}: {setTodo: (input: AnnotationToDo) => void}) => {
   const [selectedCategory, setSelectedCategory] = useState<
     ToDoActionCategory | undefined
-  >("Further Practice");
-
+  >("Ask Classmate");
+  const [todoText, setTodoText] = useState<string>("");
+  const [dueDate, setDueDate] = useState<string>("");
   const handleCategoryChange = (
     event: React.ChangeEvent<HTMLSelectElement>
   ) => {
     setSelectedCategory(event.target.value as ToDoActionCategory);
   };
 
-  const handleDueDateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    // handle due date change here
-  };
-
   return (
-    <div>
-      <label htmlFor="category">Category:</label>
+    <div className="p-4 bg-white shadow-md rounded-md">
+      <label className="block text-sm font-medium text-gray-700 mb-2">
+        To Do item
+      </label>
+      <input
+        type="text"
+        placeholder="Enter to do item here"
+        className="p-2 border rounded-md w-full"
+        value={todoText}
+        onChange={(e) => setTodoText(e.target.value)}
+      />
+
+      <label
+        htmlFor="category"
+        className="block text-sm font-medium text-gray-700 mt-4 mb-2"
+      >
+        Category:
+      </label>
       <select
         id="category"
         value={selectedCategory}
         onChange={handleCategoryChange}
+        className="p-2 border rounded-md w-full"
       >
         <option value={undefined}>Select a category</option>
         {ToDoActions.map((action) => (
@@ -47,14 +55,31 @@ const TodoCard = () => {
           </option>
         ))}
       </select>
-      <br />
-      <label htmlFor="dueDate">Due Date:</label>
+
+      <label
+        htmlFor="dueDate"
+        className="block text-sm font-medium text-gray-700 mt-4 mb-2"
+      >
+        Due Date:
+      </label>
       <input
         type="date"
         id="dueDate"
-        onChange={handleDueDateChange}
-        value={"Enter due date here"}
+        value={dueDate}
+        className="p-2 border rounded-md w-full"
       />
+
+      <button
+        onClick={() =>
+          setTodo({
+            todo: todoText,
+            category: selectedCategory! as ToDoActionCategory,
+            dueDate: new Date(dueDate),
+          })
+        }
+      >
+        Save
+      </button>
     </div>
   );
 };
