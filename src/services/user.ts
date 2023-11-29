80b8d1c31e7b7f92b1348580844f828a70b7bdff
aaronzheng <aaron.zheng@monash.edu>
2023-11-29 23:34:20 +1100

    fixing the google doc functional issues

--- src/services/user.ts
+++ src/services/user.ts
@@ -13,7 +13,7 @@ const annotation: Annotation[] = [{
         "parentTagName": "DIV",
         "parentIndex": 78,
         "textOffset": 208
-    }, text: "Suffix tree construction:  Space saving on edges: 1/1.0  Transition from extension j to j+1: -Active", id: "id", url: "https://lms.monash.edu/mod/assign/view.php?id=12092526", AnnotationTag: "Strength"
+    }, text: "Suffix tree construction:  Space saving on edges: 1/1.0  Transition from extension j to j+1: -Active", id: "id", url: "https://lms.monash.edu/mod/assign/view.php?id=12092526", annotationTag: "Strength"
 }, {
     "startMeta": {
         "parentTagName": "DIV",
@@ -24,7 +24,7 @@ const annotation: Annotation[] = [{
         "parentIndex": 78,
         "textOffset": 890
     }, "text": " and edge-to-leaf labels correctly updated: 0.25/0.25",
-    "id": "cd7343b1-2a20-4ec2-b2a0-9d92eaf35a67", url: "https://lms.monash.edu/mod/assign/view.php?id=12092526", AnnotationTag: "Weakness"
+    "id": "cd7343b1-2a20-4ec2-b2a0-9d92eaf35a67", url: "https://lms.monash.edu/mod/assign/view.php?id=12092526", annotationTag: "Weakness"
 }]
 const feedback: Feedback = { feedback: "Good", marker: "Bob", annotations: [{ annotation: annotation[0] }, { annotation: annotation[1] }] }
 const assignments: Assignment[] = [{ assignmentName: "Assignment 1", feedback: feedback, rubric: "rubric" }]
