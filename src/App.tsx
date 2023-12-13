3c608b2b674b1c6cfc5cf4fede2da8c25db33dbc
cooi0004 <cooi0004@student.monash.edu>
2023-12-13 17:17:46 +1100

    fix: collapse on google docs

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,22 @@ import {useState} from "react";
 
 function App() {
   const [collapsed, setCollapsed] = useState(true);
+  const sidebarWidth = "428px";
+  const nav = document.querySelector("nav") as HTMLElement;
+  const docs = document.querySelector("#docs-chrome") as HTMLElement;
+  if (nav) {
+    nav.style.marginRight = collapsed ? "40px" : sidebarWidth;
+  }
+  if (docs) {
+    const docContainer = document.querySelector(
+      ".kix-appview-editor-container"
+    ) as HTMLElement;
+    docContainer.style.width = "calc(100% - 428px)";
+    docContainer.style.width = collapsed ? "" : "calc(100% - 428px)";
+  }
+  document.body.style.marginRight = collapsed ? "40px" : sidebarWidth;
 
   const toggleSidebar = () => {
-    const sidebarWidth = "428px";
-    const nav = document.querySelector("nav") as HTMLElement;
-    document.body.style.marginRight = collapsed ? sidebarWidth : "40px";
-    nav ? (nav.style.marginRight = collapsed ? sidebarWidth : "40px") : null;
     setCollapsed(!collapsed);
   };
   return (
