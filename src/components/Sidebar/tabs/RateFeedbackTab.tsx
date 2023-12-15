a12c2d311e7d4c5a1e35f2c32acdece61a26dcf2
cooi0004 <cooi0004@student.monash.edu>
2023-12-15 12:39:51 +1100

    feat: inverte icon when pressed

--- src/components/Sidebar/tabs/RateFeedbackTab.tsx
+++ src/components/Sidebar/tabs/RateFeedbackTab.tsx
@@ -1,10 +1,56 @@
-import {emoticons} from "../../AnnotationIcons";
+import {emoticons, emoticonsInversed} from "../../AnnotationIcons";
 
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 
 export const RateFeedbackTab = () => {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [feedbackClarity, setFeedbackClarity] = useState(0);
+  const [feedbackPersonalised, setFeedbackPersonalised] = useState(0);
+  const [feedbackEvaluativeJudgement, setFeedbackEvaluativeJudgement] =
+    useState(0);
+  const [feedbackUsability, setFeedbackUsability] = useState(0);
+  const [feedbackEmotion, setFeedbackEmotion] = useState(0);
+  const [isSubmitted, setIsSubmitted] = useState(false);
 
+  const rateFeedbackStatements = [
+    "is easy to understand",
+    "relates to my work",
+    "helps me to critically evaluate my work",
+    "can be used even after this unit",
+    "makes me feel positive",
+  ];
+  const rateFeedbackStatmentsFunction = [
+    setFeedbackClarity,
+    setFeedbackPersonalised,
+    setFeedbackEvaluativeJudgement,
+    setFeedbackUsability,
+    setFeedbackEmotion,
+  ];
+  const rating = [
+    feedbackClarity,
+    feedbackPersonalised,
+    feedbackEvaluativeJudgement,
+    feedbackUsability,
+    feedbackEmotion,
+  ];
+  const colorToRating = (color: string) => {
+    switch (color) {
+      case "red":
+        return 1;
+      case "orange":
+        return 2;
+      case "yellow":
+        return 3;
+      case "green":
+        return 4;
+      default:
+        return 10;
+    }
+  };
+  const handleEmoticonClick = (ratingIndex: number, color: string) => {
+    const rating = colorToRating(color);
+    rateFeedbackStatmentsFunction[ratingIndex](rating);
+  };
   const toggleDropdown = () => {
     setIsDropdownOpen(!isDropdownOpen);
   };
@@ -40,7 +86,36 @@ export const RateFeedbackTab = () => {
       />
     </svg>
   );
+  const handleSubmit = () => {
+    if (
+      feedbackClarity &&
+      feedbackPersonalised &&
+      feedbackEvaluativeJudgement &&
+      feedbackUsability &&
+      feedbackEmotion
+    ) {
+      const feedback = {
+        feedbackClarity,
+        feedbackPersonalised,
+        feedbackEvaluativeJudgement,
+        feedbackUsability,
+        feedbackEmotion,
+      };
+      console.log(feedback);
+      setIsSubmitted(true);
+      setTimeout(() => setIsSubmitted(false), 3000);
+    }
+  };
 
+  useEffect(() => {
+    handleSubmit();
+  }, [
+    feedbackClarity,
+    feedbackPersonalised,
+    feedbackEvaluativeJudgement,
+    feedbackUsability,
+    feedbackEmotion,
+  ]);
   return (
     <div className="border rounded-lg">
       <button
@@ -51,24 +126,26 @@ export const RateFeedbackTab = () => {
         {isDropdownOpen ? chevronIconUp : chevronIconDown}
       </button>
       {isDropdownOpen &&
-        [
-          "is easy to understand",
-          "relates to my work",
-          "helps me to critically evaluate my work",
-          "can be used even after this unit",
-          "makes me feel positive",
-        ].map((feedback, index) => (
+        rateFeedbackStatements.map((feedback, index) => (
           <div
             key={index}
             className="flex justify-between items-center mb-2 p-2"
           >
             <span className="flex-1">{feedback}</span>
             <div className="flex items-center">
-              {Object.entries(emoticons).map(([category, counter]) => (
-                <button key={category} className="ml-2 bg-blue-500">
+              {Object.entries(emoticons).map(([color, icon]) => (
+                <button
+                  key={color}
+                  className="ml-2"
+                  onClick={() => handleEmoticonClick(index, color)}
+                >
                   <img
-                    src={counter}
-                    alt={category}
+                    src={
+                      rating[index] === colorToRating(color)
+                        ? emoticonsInversed[color]
+                        : icon
+                    }
+                    alt={color}
                     style={{width: 40, height: 40}}
                   />
                 </button>
@@ -76,6 +153,21 @@ export const RateFeedbackTab = () => {
             </div>
           </div>
         ))}
+      {isSubmitted && (
+        <div
+          style={{
+            position: "fixed",
+            bottom: "20px",
+            right: "20px",
+            backgroundColor: "black",
+            color: "white",
+            padding: "10px",
+            borderRadius: "5px",
+          }}
+        >
+          Your feedback rating has been submitted. Thank you!
+        </div>
+      )}
     </div>
   );
 };
